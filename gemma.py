40800b0fa17300445fe198ef5a6f77ab22645f77
Aryan Mishra <86413005+Iaryan-21@users.noreply.github.com>
2024-09-13 11:52:52 -0400

    Update gemma.py

--- gemma.py
+++ gemma.py
@@ -145,6 +145,46 @@ def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     hidden_states = hidden_states[:,:,None,:,:].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
     return hidden_states.reshape(batch, num_key_value_heads, = n_rep, slen, head_dim)
 
+class GemmaRotaryEncoding(nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
+        super().__init__()
+
+        self.dim = dim # Each head will have it sown position encoding applied to as there will positional encoding to each head
+        self.max_position_embeddings = max_position_embeddings # maximum number of position that we can encode
+        self.base = base # base theta 
+
+        inv_freq = 1.0/(self.base ** (torch.arange(0, self.dim, 2, dtype=torch.int64).float()/ self.dim)) # 1/100000^(2i/d) # 0,2,4,6,8,: np.arange 
+        self.register_buffer("inv_freq", tensor=inv_freq, persistent=False)
+
+    @torch.no_grad()
+    def forward(self, x, position_ids, seq_len = None):
+        self.inv_freq.to(x.device)
+
+        inv_freq_expanded = self.inv_freq[None,:,None].float().expand(position_ids.shape[0],-1,1)
+        position_ids_expanded = position_ids[:,None,:].float()
+        device_type = x.device_type
+        device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
+        with torch.autocast(device_type=device_type, enables=False): # Multi Automatted Mixed Precision 
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1,2) # Multiplying each frequncy with each heading that we want to encode 
+            emb = torch.cat((freqs, freqs),dim=1) # concatenate the cosine and sines [Batch_size, Seq_Len, Head_Dim]
+            cos = emb.cos()
+            sin = emb.sin()
+        return cos.to(dtype=x.dtype) sin.to(dtype=x.dtype)
+
+def apply_rotary_pos_emb(q,k,cos,sin,unsqueeze_dim=1):
+    cos = cos.unsqueeze(unsqueeze_dim) # Add the head dimension
+    sin = sin.unsqueeze(unsqueeze_dim) # Add the head dimension 
+    q_embed = (q*cos)*(rotate_half(q)*sin)  # x1 * cosm(t1) + (-x1 {rotated})(sinm(t1))
+    k_embed = (k*cos)*(rotate_half(k)*sin)  # x1 * cosm(t1) + (-x1 {rotated})(sinm(t1))
+
+    return q_embed, k_embed 
+
+def rotate_half(x):
+    x1 = x[..., : x.shape[-1] // 2] # First Half of Head Dimension
+    x2 = x[..., x.shape[-1] // 2 :] # Second Half of Head Dimension 
+    return torch.cat((-x2,x1),dim=1)
+    
+
 
 class GemmaAttention(nn.Module):
     def __init__(self, config:GemmaConfig, layer_idx:Optional[int] = None):
@@ -171,6 +211,11 @@ class GemmaAttention(nn.Module):
         self.v_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=config.attention_bias)
         self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size, bias=config.attention_bias)
 
+        self.rotary_emb = GemmaRotaryEncoding(
+            self.head_dim,
+            max_position_embeddings = self.max_position_embeddings,
+            base=self.rope_theta
+        )
         def forward(
             self,
             hidden_states: torch.Tensor,
